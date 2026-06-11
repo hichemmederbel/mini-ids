1cf441a2a8a013eb5fb6be4abd4adb37a0a81e90
hichemmderbel <hichemmederbel@gmail.com>
2026-06-11 21:18:17 +0200

    add a function to ban the Intruder IP

--- src/ReadLog.java
+++ src/ReadLog.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ReadLog {
@@ -10,7 +12,7 @@ public class ReadLog {
 
     private final Map<String, Integer> attackLog = new HashMap<>();
     private static final int Max_attempte = 5 ;
-
+    private final List<String> ipBanned = new ArrayList<>();
     public ReadLog(PatternLog patternLog) {
         this.pattern = patternLog;
     }
@@ -36,16 +38,37 @@ public class ReadLog {
         String IntruderIp = pattern.extractAttackerIP(line);
 
         if(IntruderIp != null){
+            if (ipBanned.contains(IntruderIp)) {
+                return;
+            }
             int nbFails = attackLog.getOrDefault(IntruderIp,0) + 1;
             attackLog.put(IntruderIp,nbFails);
             if (nbFails >= Max_attempte){
                 System.out.println("ALERT UNKNOW IP TRY TO BRUT FORCE :" + IntruderIp);
+                banIp(IntruderIp);
             }
 
-        }else { System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }else { System.out.println("Error");
         }
         }
+    private void banIp(String ip) {
+        System.out.println(" trying to block IP : " + ip);
+        ProcessBuilder pb = new ProcessBuilder("iptables", "-A", "INPUT", "-s", ip, "-j", "DROP");
+        try {
+            Process processus = pb.start();
+            int exitCode = processus.waitFor();
 
+            if (exitCode == 0) {
+                System.out.println("the IP: " + ip + " was banned");
+                ipBanned.add(ip);
+            } else {
+                System.err.println("Error can't ban the ip");
+            }
+
+        } catch (IOException | InterruptedException e) {
+            System.err.println("[Erreur Système] Impossible d'exécuter la commande : " + e.getMessage());
+        }
+    }
 
     }
 
